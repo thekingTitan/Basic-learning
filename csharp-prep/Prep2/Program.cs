58e8e086f465916e26d8f3ccab2a575bee54e0e5
kingtitan <igw22003@byui.edu>
2024-04-23 10:54:24 -0700

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+         Console.Write("what is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter="";
+
+        if (number >= 90 )
+        {
+            letter="A";
+
+        }
+        else if (number >= 80)
+        {
+            letter="B";
+        }
+        else if (number >= 70)
+        {
+            letter="C";
+        }
+        else if (number >= 60)
+        {
+            letter="D";
+        }
+        else 
+        {
+            letter="F";
+        }
+        Console.WriteLine($"your grade is {letter}");
+        if (number >= 70)
+        {
+            Console.WriteLine("you passed!");
+        }
+        else
+        {
+            Console.WriteLine("good Luck Next time");
+        }
     }
+    
 }
+    
+
+    
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("what is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter="";
+
+        if (number >= 90 )
+        {
+            letter="A";
+
+        }
+        else if (number >= 80)
+        {
+            letter="B";
+        }
+        else if (number >= 70)
+        {
+            letter="C";
+        }
+        else if (number >= 60)
+        {
+            letter="D";
+        }
+        else 
+        {
+            letter="F";
+        }
+        Console.WriteLine($"your grade is {letter}");
+        if (number >= 70)
+        {
+            Console.WriteLine("you passed!");
+        }
+        else
+        {
+            Console.WriteLine("good Luck Next time");
+        }
     }
+    
 }
