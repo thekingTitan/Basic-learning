32cd3de996fade03a1f226a7c5e8f9760f17fe17
kingtitan <igw22003@byui.edu>
2024-04-27 19:52:59 -0700

    finished all

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,41 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("what is your grade percentage? ");
-        string grade = Console.ReadLine();
-        int number = int.Parse(grade);
-        string letter="";
+        Console.Write("what is your magic number?");
+        string number = Console.ReadLine();
+        int numbers = int.Parse(number);
+        Console.Write("what is your guess?");
 
-        if (number >= 90 )
-        {
-            letter="A";
-
-        }
-        else if (number >= 80)
-        {
-            letter="B";
-        }
-        else if (number >= 70)
-        {
-            letter="C";
-        }
-        else if (number >= 60)
-        {
-            letter="D";
-        }
-        else 
-        {
-            letter="F";
-        }
-        Console.WriteLine($"your grade is {letter}");
-        if (number >= 70)
-        {
-            Console.WriteLine("you passed!");
-        }
-        else
-        {
-            Console.WriteLine("good Luck Next time");
-        }
+       Console.Write("")
     }
+       
     
 }
